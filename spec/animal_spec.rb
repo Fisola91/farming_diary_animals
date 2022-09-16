4679889f0e95c9cdf971e9f2cf47fedfc733f64f
Fisola Adesuyi <adesuyifisola@gmail.com>
2022-09-16 18:04:11 +0200

    code modified

--- spec/animal_spec.rb
+++ spec/animal_spec.rb
@@ -14,8 +14,6 @@ RSpec.describe Animal do
 
       it "should define specific methods" do
         expect(Chicken.instance_methods(false)).to include(:talk)
-        expect(Chicken.instance_methods(false)).to include(:gender)
-        expect(Chicken.instance_methods(false)).to include(:eggs)
       end
 
       it "should duplicate the ´Chicken´ shared methods methods" do
@@ -30,13 +28,18 @@ RSpec.describe Animal do
 
       it "should define specific methods" do
         expect(Cow.instance_methods(false)).to include(:talk)
-        expect(Cow.instance_methods(false)).to include(:milk)
       end
 
       it "should duplicate the ´Chicken´ shared methods methods" do
         expect(Cow.instance_methods(false)).to include(:feed!)
       end
+    end
 
+    context "Animal" do
+      it "should increase animal energy by 1" do
+        animal.feed!
+        expect(animal.instance_variable_get(:@energy)).to eq(1)
+      end
     end
   end
 
